22796e7f16a0ad29437a820ecc55f78840878fea
dirgaastya <astya444@gmail.com>
2022-05-05 15:25:16 +0700

    create model , model relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+    protected $fillable = ['id','title', 'body'];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class)->withDefault('Admin User');
+    }
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    } 
+
+    public function tags():BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag');
+    }
 }
